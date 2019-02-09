3a524318a0dd5cff82d755a2e8ea25e79a4e8f44
arao <araobp@users.noreply.github.com>
2019-02-09 17:39:04 +0900

    MFSCs of music instruments

--- oscilloscope/run_browser_music.bat
+++ oscilloscope/run_browser_music.bat
@@ -1,2 +1,2 @@
 set PYTHONPATH=%PYTHONPATH%;./script
-python ./script/oscilloscope.py COM15 --browser --dataset ../dataset/data_music2
+python ./script/oscilloscope.py COM15 --browser --dataset ../dataset/data_music
