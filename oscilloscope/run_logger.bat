66bbda230bee6ff6c9c3c721ff79544aa4e15f86
arao <araobp@users.noreply.github.com>
2019-04-02 17:36:43 +0900

    update

--- oscilloscope/run_logger.bat
+++ oscilloscope/run_logger.bat
@@ -1,2 +1,2 @@
 set PYTHONPATH=%PYTHONPATH%;./script
-python ./script/logger.py --port COM15 --dataset_folder ../dataset/data_my_home --time 180 --section 15
+python ./script/logger.py --port COM15 --dataset_folder ../dataset/data_my_home --time 600 --section 20
