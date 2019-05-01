f15b373bbae8d090afc1dd585a411571e4aec9ce
arao <araobp@users.noreply.github.com>
2019-05-02 08:06:27 +0900

    update

---
+++ spa/use_cases/class_labels_my_home.js
@@ -0,0 +1,21 @@
+const useCase = "My home";
+
+const devices = ["BLE1", "BLE2", "BLE3"];
+
+const classLabels = {
+  0: "chatting",
+  1: "reading a book",
+  2: "watching TV",
+  3: "cocking",
+  4: "hamster grinding teeth",
+  5: "silence",
+  6: "vacuum cleaner",
+  7: "showering",
+  8: "washing machine",
+  9: "doing the dishes",
+  10: "walking the room",
+  11: "playing the piano",
+  12: "going up or down the stairs",
+  13: "eating snack"
+}
+
